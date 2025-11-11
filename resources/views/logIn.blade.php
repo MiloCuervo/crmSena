795f50895487ebacea343406c53874a29db29232
MiloCuervo <Camilo.cuervojobs@gmail.com>
2025-11-11 15:10:41 -0500

    nada

--- resources/views/logIn.blade.php
+++ resources/views/logIn.blade.php
@@ -4,7 +4,7 @@
         <title>Log In</title>
         <meta charset='UTF-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>        
-        <link rel="stylesheet" href="{{url('css/Home.css')}}">
+        <link rel="stylesheet" href="css/logIn.css">
     </head>
     <body>
         <div class='CuerpoPagina'>
